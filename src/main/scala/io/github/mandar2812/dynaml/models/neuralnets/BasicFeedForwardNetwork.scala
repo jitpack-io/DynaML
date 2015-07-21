66fdcfbbc34d6156af8645971a03b4db580d7439
mandar2812 <mandar2812@gmail.com>
2015-07-21 02:04:35 +0200

    Laying groundwork for Neural Nets: Created frames for neurons, synapses & input patterns. BasicFeedForwardNetwork [WIP]

---
+++ src/main/scala/io/github/mandar2812/dynaml/models/neuralnets/BasicFeedForwardNetwork.scala
@@ -0,0 +1,26 @@
+package io.github.mandar2812.dynaml.models.neuralnets
+
+import breeze.linalg.DenseVector
+import com.tinkerpop.blueprints.Graph
+import com.tinkerpop.frames.FramedGraph
+
+/**
+ * Represents the template of a Feed Forward Neural Network
+ * backed by an underlying graph.
+ */
+abstract class BasicFeedForwardNetwork[G]
+  extends NeuralNetwork[G, FramedGraph[Graph]]{
+  
+
+}
+
+object BasicFeedForwardNetwork {
+  def feedForwardFunc(networkGraph: FramedGraph[Graph])
+                     (inputPattern: DenseVector[Double]): Unit = {}
+
+  def initializeWeights(hiddenLayers: Int,
+                        inputDimensions: Int,
+                        outputDimensions: Int,
+                        neuronCounts: List[Int]) = {}
+
+}
